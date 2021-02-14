08a1abca09e5bdb0355f98bc9492821d59332e46
Daniel Obot <danobot@hotmail.com>
2021-02-14 15:17:31 -0600

    feat: player now can select a card from opponent

--- app.py
+++ app.py
@@ -7,24 +7,35 @@ def main():
     """Entry point for the application"""
     print('Hello world')
     game_deck = Deck()
-    player = Hand()
+    human_player = Hand()
     computer_player = Hand()
     game_deck.shuffle()
     game_over = False
 
-    player.cards = game_deck.deal_hand()
+    human_player.cards = game_deck.deal_hand()
     computer_player.cards = game_deck.deal_hand()
 
     while not game_over:
 
-        player_card_selection = input(f'Please choose a card from your hand you have the following: {player}')
-        while not player.check_if_in_hand(player_card_selection):
-            print(f'{player_card_selection} not in hand.')
-            player_card_selection = input(f'Please choose a card from your hand you have the following: {player}')
+        player_card_selection = input(f'Please choose a card from your hand you have the following: {human_player}')
+        print(f'Computer has: {computer_player}')  # delete me later
+        players_turn = True
+        while players_turn:
+            while not human_player.check_if_in_hand(player_card_selection.upper()):
+                print(f'{player_card_selection} not in hand.')
+                player_card_selection = input(
+                    f'Please choose a card from your hand you have the following: {human_player}')
+                print(f'Computer has: {computer_player}')  # delete me later
 
-        print('Yes')
+            if computer_player.check_if_in_hand(player_card_selection.upper()):
+                print(f'Computer has those cards!')
+                human_player.cards = computer_player.draw_card(player_card_selection, human_player.cards)
+            else:
+                print('Go fish!')
+                human_player.cards.append(game_deck.draw_card())
+                players_turn = False
 
-    print(game_deck)
+    print(human_player)
     print(computer_player)
 
 
